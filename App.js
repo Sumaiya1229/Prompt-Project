3ad2810430c567fd33384f0d07483c2e8d4a471a
Sumaiya Amin <48669010+Sumaiya1229@users.noreply.github.com>
2021-05-07 16:30:52 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import { StyleSheet, View, StatusBar } from "react-native";
-import WelcomeScreenNavigator from './src/navigation/welcomeScreenNavigation';
+import RootNavigator from './src/navigation/RootNavigator';
 import { NavigationContainer } from "@react-navigation/native";
 import AccountTypePrompt from './src/components/userAuth/userAuthCommon/accountTypePrompt';
 import SpProviderBottomNav from './src/navigation/spProviderBottomNav';
 import { Provider } from "react-redux";
 import store from './src/redux/index';
-import Landing from "./src/landing";
 import ClientBottomNav from './src/navigation/clientBottomNav';
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Provider store={store}>
-        {/* <View style={styles.statusbar}></View> */}
-        {/* <Landing/> */}
-        <ClientBottomNav/>
-        {/* <SpProviderBottomNav/> */}
+        <RootNavigator/>
       </Provider>
     </View>
   );
